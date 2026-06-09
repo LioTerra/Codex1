d2cba9f32193dd760a3488ba711d956374256500
Leonardo Terra <leonardoterra_@hotmail.com>
2026-06-09 15:36:25 -0300

    Initial landing page

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import LandingPage from './LandingPage_com_prints_depoimentos'
+
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <LandingPage />
+  </React.StrictMode>,
+)
